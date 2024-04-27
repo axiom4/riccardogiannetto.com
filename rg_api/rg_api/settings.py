06db4553674945374364244f72679857b56a01f4
Riccardo GIannetto <rgiannett@gmail.com>
2024-04-27 21:14:48 +0200

    upgrade service worker

--- rg_api/rg_api/settings.py
+++ rg_api/rg_api/settings.py
@@ -199,6 +199,7 @@ CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
         "LOCATION": "127.0.0.1:11211",
+        "KEY_PREFIX": 'gallery_'
     }
 }
 
