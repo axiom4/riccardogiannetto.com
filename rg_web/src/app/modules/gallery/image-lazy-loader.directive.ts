142b8021a7716e389aa60dc015dadaf089eedae2
Riccardo GIannetto <rgiannett@gmail.com>
2024-05-02 20:24:18 +0200

    lazy image loading

--- rg_web/src/app/modules/gallery/image-lazy-loader.directive.ts
+++ rg_web/src/app/modules/gallery/image-lazy-loader.directive.ts
@@ -3,7 +3,9 @@ import {
   Directive,
   ElementRef,
   HostBinding,
+  HostListener,
   Input,
+  Renderer2,
 } from '@angular/core';
 
 @Directive({
@@ -14,7 +16,37 @@ export class ImageLazyLoaderDirective implements AfterViewInit {
   @HostBinding('attr.src') srcAttr: string | undefined;
   @Input() src: string | undefined;
 
-  constructor(private el: ElementRef) {}
+  private _loading = true;
+  private _error = false;
+
+  get error(): boolean {
+    return this._error;
+  }
+
+  get loading(): boolean {
+    return this._loading;
+  }
+  set loading(val: boolean) {
+    if (val) this.renderer.addClass(this.el.nativeElement, 'loading');
+    else this.renderer.removeClass(this.el.nativeElement, 'loading');
+    this._loading = val;
+  }
+
+  @HostListener('loadstart') onLoadStart() {
+    this.loading = true;
+  }
+
+  @HostListener('load') onLoad() {
+    this.loading = false;
+    this.renderer.addClass(this.el.nativeElement, 'loaded');
+  }
+  @HostListener('error') onError() {
+    this.loading = false;
+    this._error = true;
+    this.renderer.addClass(this.el.nativeElement, 'error');
+  }
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngAfterViewInit() {
     this.canLazyLoad() ? this.lazyLoadImage() : this.loadImage();
