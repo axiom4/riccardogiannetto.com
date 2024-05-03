a3eb45275ca66103832b4592c9cd515d7dfa391a
Riccardo GIannetto <rgiannett@gmail.com>
2024-05-03 22:51:25 +0200

    remove test key

--- rg_web/src/environments/environment.development.ts
+++ rg_web/src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   api_url: 'http://127.0.0.1:8000',
   production: false,
-  pexelsApiKey: 'z0H7FZEybnSkcVf33hrH4Txfjbx4zYHV3Hy5r7tegNWW0Rgzve1hZ6ni',
 };
--- rg_web/src/environments/environment.ts
+++ rg_web/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   api_url: 'https://www.riccardogiannetto.com/api',
   production: true,
-  pexelsApiKey: 'z0H7FZEybnSkcVf33hrH4Txfjbx4zYHV3Hy5r7tegNWW0Rgzve1hZ6ni',
 };
