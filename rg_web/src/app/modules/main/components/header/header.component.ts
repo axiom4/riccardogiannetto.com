3d22f9c3da64ec3d845de37426dae431055de132
Riccardo GIannetto <rgiannetto@gmail.com>
2026-01-18 12:36:11 +0100

    fix: enhance header branding loading behavior with opacity transition

--- rg_web/src/app/modules/main/components/header/header.component.ts
+++ rg_web/src/app/modules/main/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, PLATFORM_ID, signal, viewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,25 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   menu = viewChild<ElementRef>('menu');
+  isLoaded = signal(false);
+  private platformId = inject(PLATFORM_ID);
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Use requestAnimationFrame to ensure we are in the next paint frame after init
+      // Or window.onload if strictly "page load" is required, but usually 
+      // minimal delay after view init is better for SPA feeling.
+      
+      // Let's use a small timeout to allow browser to parse SVG/Fonts
+      setTimeout(() => {
+        this.isLoaded.set(true);
+      }, 100);
+    } else {
+      this.isLoaded.set(true);
+    }
+  }
 
   toggleMenu() {
     console.log('toggle menu');
