14e8ca4837de6d6b4036a18ba46ec88edeade09c
Riccardo GIannetto <rgiannetto@gmail.com>
2026-01-18 11:52:02 +0100

    feat: implement font loading logic and update header component for dynamic styling

--- rg_web/src/app/modules/main/components/header/header.component.ts
+++ rg_web/src/app/modules/main/components/header/header.component.ts
@@ -1,5 +1,14 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  PLATFORM_ID,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +16,22 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   menu = viewChild<ElementRef>('menu');
+  fontsLoaded = signal(false);
+  private platformId = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      document.fonts.load('1em myfantasy').then(() => {
+        this.fontsLoaded.set(true);
+      });
+    } else {
+      // In SSR we can't verify, but we can default to true to avoid empty header
+      this.fontsLoaded.set(true);
+    }
+  }
+
   toggleMenu() {
     console.log('toggle menu');
     this.menu()?.nativeElement.classList.toggle('visible');
