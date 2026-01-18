387cbc2383c97a2e6851ed4360fc8867f15a1b7d
Riccardo GIannetto <rgiannetto@gmail.com>
2026-01-18 19:44:18 +0100

    feat: add truncate functionality for UserActivity in admin interface

--- rg_api/analytics/admin.py
+++ rg_api/analytics/admin.py
@@ -62,6 +62,29 @@ class UserActivityAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'path', 'ip_address', 'city', 'country')
     readonly_fields = ('timestamp', 'latitude', 'longitude', 'city', 'country')
 
+    change_list_template = 'admin/analytics/useractivity/change_list.html'
+
+    def get_urls(self):
+        from django.urls import path
+        urls = super().get_urls()
+        custom_urls = [
+            path('truncate/', self.truncate_view, name='analytics_useractivity_truncate'),
+        ]
+        return custom_urls + urls
+
+    def truncate_view(self, request):
+        from django.shortcuts import redirect
+        from django.contrib import messages
+        
+        if request.user.is_superuser:
+            count = UserActivity.objects.count()
+            UserActivity.objects.all().delete()
+            self.message_user(request, f"Successfully deleted {count} activities.", messages.SUCCESS)
+        else:
+            self.message_user(request, "You do not have permission to perform this action.", messages.ERROR)
+            
+        return redirect('admin:analytics_useractivity_changelist')
+
     def session_link(self, obj):
         if obj.session:
             from django.utils.html import format_html
