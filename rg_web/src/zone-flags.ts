56e288cdb14e33956751261bd6bb65690570e4ae
Riccardo GIannetto <rgiannetto@gmail.com>
2026-01-30 06:21:53 +0100

    fix: refactor zone-flags.ts to improve type safety and structure

--- rg_web/src/zone-flags.ts
+++ rg_web/src/zone-flags.ts
@@ -1,1 +1,9 @@
-(window as any).__zone_symbol__PASSIVE_EVENTS = ['touchstart', 'wheel'];
+export {};
+
+declare global {
+  interface Window {
+    __zone_symbol__PASSIVE_EVENTS: string[];
+  }
+}
+
+window.__zone_symbol__PASSIVE_EVENTS = ['touchstart', 'wheel'];
