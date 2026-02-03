ff3d6337a710d8cc5cf02dbeebdbc33b9ef16a92
Riccardo GIannetto <rgiannetto@gmail.com>
2026-02-03 06:16:42 +0100

    refactor: split gallery tests into dedicated files for API and models.

--- rg_api/gallery/tests/test_models.py
+++ rg_api/gallery/tests/test_models.py
@@ -1,14 +1,10 @@
 import tempfile
-import shutil
 from unittest.mock import patch
 from PIL import Image
 
 from django.test import TestCase, override_settings
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.urls import reverse
-from rest_framework.test import APITestCase
-from rest_framework import status
 
 from gallery.models import Gallery, ImageGallery
 
@@ -105,33 +101,3 @@ class ImageGalleryModelTest(TestCase):
         self.assertEqual(image_gallery.latitude, 41.9028)
         self.assertEqual(image_gallery.longitude, 12.4964)
         self.assertEqual(image_gallery.shutter_speed, 0.01)
-
-
-@override_settings(
-    DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory:'}},
-    CACHES={'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}}
-)
-class GalleryAPITest(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='testapi', password='password')
-        self.gallery = Gallery.objects.create(
-            title='API Gallery',
-            tag='api-gallery',
-            author=self.user
-        )
-
-    def test_list_galleries(self):
-        url = '/portfolio/galleries'
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Handle Pagination
-        results = response.data.get('results', response.data)
-        self.assertEqual(len(results), 1)
-        self.assertEqual(results[0]['title'], 'API Gallery')
-
-    def test_retrieve_gallery(self):
-        response = self.client.get(f'/portfolio/galleries/{self.gallery.id}')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], 'API Gallery')
