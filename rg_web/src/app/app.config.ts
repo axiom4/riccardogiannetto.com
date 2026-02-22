52018737c0d75910731ad6cf7d398023b2c16d2a
Riccardo GIannetto <rgiannetto@gmail.com>
2026-02-22 09:44:53 +0100

    fix(config): remove unused zone-flags and update polyfills configuration

--- rg_web/src/app/app.config.ts
+++ rg_web/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -17,7 +17,7 @@ export function apiConfigFactory(): Configuration {
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(
       routes,
       withInMemoryScrolling({
--- rg_web/src/zone-flags.ts
+++
@@ -1,9 +0,0 @@
-export {};
-
-declare global {
-  interface Window {
-    __zone_symbol__PASSIVE_EVENTS: string[];
-  }
-}
-
-window.__zone_symbol__PASSIVE_EVENTS = ['touchstart'];
