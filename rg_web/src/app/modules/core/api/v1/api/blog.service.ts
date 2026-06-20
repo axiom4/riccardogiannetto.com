72bb36698bea94a5a00622edce0c7dce54ceb39d
Riccardo GIannetto <rgiannetto@gmail.com>
2026-06-20 07:16:22 +0200

    refactor: remove unused HttpParams and COLLECTION_FORMATS imports from API services

--- rg_web/src/app/modules/core/api/v1/api/blog.service.ts
+++ rg_web/src/app/modules/core/api/v1/api/blog.service.ts
@@ -10,8 +10,8 @@
 /* tslint:disable:no-unused-variable member-ordering */
 
 import { Inject, Injectable, Optional }                      from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams,
-         HttpResponse, HttpEvent, HttpContext 
+import { HttpClient,
+         HttpResponse, HttpEvent, HttpContext
         }       from '@angular/common/http';
 import { Observable }                                        from 'rxjs';
 import { OpenApiHttpParams, QueryParamStyle } from '../query.params';
@@ -30,7 +30,7 @@ import { PaginatedPostPreviewList } from '../model/paginatedPostPreviewList';
 import { Post } from '../model/post';
 
 // @ts-ignore
-import { BASE_PATH, COLLECTION_FORMATS }                     from '../variables';
+import { BASE_PATH }                     from '../variables';
 import { Configuration }                                     from '../configuration';
 import { BaseService } from '../api.base.service';
 import {
